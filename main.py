9e58a8a4d49980f5325c28de18552f9d5472c8f4
chloek409 <chloek9649@gmail.com>
2024-07-17 14:15:49 +0900

    add recommendation

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# main.py
 import cv2
-
 import threading
-
 from face_detection import FaceDetection
 from model_operation import Predictor
-from recommendation import ProductRecommendationAlgorithm
+from recommendation import RecommendationAlgorithm
 import tensorflow as tf
 import os
+
+
 def main():
 
     face_detection = FaceDetection(camera_index=0)
@@ -20,7 +19,7 @@ def main():
     if os.path.isfile(age_model_path) and os.path.isfile(gender_model_path):
         model_operation = Predictor(age_model_path,gender_model_path,env="WINDOW")
 
-    product_recommendation_algorithm = ProductRecommendationAlgorithm(model_path="path/to/product_recommendation_model")
+    recommendation = RecommendationAlgorithm()
 
     try:
         while True:
@@ -33,13 +32,22 @@ def main():
                 # Convert the frame back to BGR for OpenCV display
                 display_frame = cv2.cvtColor(org_frame, cv2.COLOR_RGB2BGR)
                 
-                age_prediect,gender_predict = model_operation.predict_image(face_frames)
+                age_prediect, gender_predict = model_operation.predict_image(face_frames)
 
                 for bbox, age, gender in zip(face_bboxes, age_prediect, gender_predict):
                     x, y, w, h = bbox
                     cv2.rectangle(display_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    text = f'Age: {age}, Gender: {"Male" if gender == 1 else "Female"}'
-                    cv2.putText(display_frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    text = f'Age: {age}, Gender: {"Male" if gender == 0 else "Female"}'
+                    cv2.putText(display_frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0,0), 2)
+                    
+                    try:
+                        recommendation.run_recommendation(age, gender)
+                        rec_res = recommendation.get_recommendation()
+                        cv2.putText(display_frame, rec_res, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 3)
+
+                    except Exception as e:
+                        print(f"Error in recommendation: {e}")
+
 
                 # Display the frame
                 cv2.imshow("Face Detection", display_frame)
--- recommendation.py
+++ recommendation.py
@@ -1,11 +1,48 @@
-# product_recommendation_algorithm.py
 
-class ProductRecommendationAlgorithm:
-    def __init__(self, model_path):
-        # Initialize product recommendation algorithm with necessary data or models
-        self.model = None  # Replace with actual initialization
+class RecommendationAlgorithm:
+    def __init__(self):
+        self.model = None
+        self.age = 0
+        self.gender = 0
+        self.recommendation = ""
     
-    def recommend_product(self, user_profile):
-        # Recommend a product based on user_profile (e.g., recognized user)
-        recommended_product = "Coffee"  # Replace with actual recommendation logic
-        return recommended_product
+    def run_recommendation(self, age, gender):
+        if age<10:
+            self.age = 0
+        elif 10<=age and age<20:
+            self.age = 10
+        elif 20<=age and age<30:
+            self.age = 20
+        elif 30<=age and age<40:
+            self.age = 30
+        elif 40<=age and age<50:
+            self.age = 40
+        elif 50<=age and age<60:
+            self.age = 50
+        else:
+            self.age = 60
+
+        self.gender = gender
+
+        recommendation_map = {
+            #(나이,성별): 추천메뉴 (아메리카노, 카페라떼 제외)
+            (0, 0): "Caffe Latte",
+            (10, 0): "Caffe Latte",
+            (20, 0): "Grapefruit Honey Black Tea",
+            (30, 0): "Dolce Cold Brew",
+            (40, 0): "Dolce Latte",
+            (50, 0): "Americano",
+            (50, 0): "Americano",
+
+            (0,0): "Caffe Latte",
+            (10, 1): "java chip frappuccino",
+            (20, 1): "Grapefruit Honey Black Tea",
+            (30, 1): "Dolce Cold Brew",
+            (40, 1): "Dolce Latte",
+            (50, 1): "Decaf Americano",
+            (50, 1): "Americano",
+        }
+        self.recommendation = recommendation_map[(self.age, self.gender)]
+    
+    def get_recommendation(self):
+        return self.recommendation
